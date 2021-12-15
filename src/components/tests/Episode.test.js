dc72b4086576c5034678bbda8fa667f1d7e42045
jcpcabanada <jcpcabanada@gmail.com>
2021-12-14 17:19:59 -0800

    2

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,33 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
-
+const testEpisode = {
+    id: 1,
+    name: "",
+    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+    season: 1,
+    number: 1,
+    summary:"test summary",
+    runtime:1
+}
+const testEpisodeNoImage = {
+    id: 1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary:"",
+    runtime:1
+}
 test("renders without error", () => {});
+render(<Episode episode={testEpisode}/>)
 
 test("renders the summary test passed as prop", ()=>{});
+render(<Episode episode={testEpisode}/>);
+const summary = screen.queryByText(/test summary/i);
+expect(summary).toBeInTheDocument();
+expect(summary).toBeTruthy();
+expect(summary).toHaveTextContent("test summmary");
 
 test("renders default image when image is not defined", ()=>{});
+render(<Episode episode={testEpisodeNoImage}/>);
